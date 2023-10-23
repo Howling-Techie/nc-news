e143fc9f05b6aa9758d05e0e3065d4a2991eb3e7
Howling Techie <howling.techie@gmail.com>
2023-10-23 12:33:16 +0100

    Installed Radix UI

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import {Theme} from "@radix-ui/themes";
+import "@radix-ui/themes/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Theme accentColor="red">
+            <body>
+            <App/>
+            </body>
+        </Theme>
+    </React.StrictMode>,
+);
