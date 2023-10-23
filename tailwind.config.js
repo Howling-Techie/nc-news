188a133c8fe02736b0494f717bc2a11b7a123e47
Howling Techie <howling.techie@gmail.com>
2023-10-23 16:11:29 +0100

    Added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./index.html",
+        "./src/**/*.{js,ts,jsx,tsx}"
+    ],
+    darkMode: "class",
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
