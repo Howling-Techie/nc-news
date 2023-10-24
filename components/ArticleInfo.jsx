c370e6cadb0218ea91bd12f1e2c4e49b86a4a0c4
Howling Techie <howling.techie@gmail.com>
2023-10-24 09:18:11 +0100

    Added more semantic HTML tags

--- components/ArticleInfo.jsx
+++ components/ArticleInfo.jsx
@@ -9,25 +9,25 @@ export const ArticleInfo = ({article}) => {
     };
 
     return (
-        <div className="flex mb-4">
-            <div className="w-1/4 pr-4">
+        <section className="flex mb-4">
+            <figure className="w-1/4 pr-4">
                 <img src={article_img_url} alt={title} className="w-full h-auto rounded-md"/>
-            </div>
-            <div className="w-3/4 flex flex-col justify-center">
+            </figure>
+            <summary className="w-3/4 flex flex-col justify-center">
                 <Link to={`/articles/${article_id}`} className="text-2xl font-bold hover:underline">{title}</Link>
-                <div className="text-sm hidden md:block py-1">
+                <details className="text-sm hidden md:block py-1">
                     <Link to={`/topics/${topic}`} className="text-gray-700 hover:underline">
                         {topic}
                     </Link>
-                </div>
-                <div className="text-gray-700 text-sm mb-2">
+                </details>
+                <details className="text-gray-700 text-sm mb-2">
                     {formatDate(created_at)} - {author}
-                </div>
-                <div className="text-sm font-bold  hidden md:block">
+                </details>
+                <details className="text-sm font-bold  hidden md:block">
                     <span role="img" aria-label="votes"> 👍 {votes} votes</span> |
                     <span role="img" aria-label="comments"> 💬 {comment_count} comments</span>
-                </div>
-            </div>
-        </div>
+                </details>
+            </summary>
+        </section>
     );
 };
